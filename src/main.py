6748fa1376e3ca6659e082ff14e4cbde4a09018a
rhpozzo <rhpozzo@gmail.com>
2020-01-23 20:37:41 -0500

    update and delete methods added

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/people', methods=['GET'])
-def handle_person():
+def handle_people():
     people = Person.query.all()
     people = list(map(lambda x: x.serialize(), people))
     return jsonify(people), 200
@@ -38,18 +38,52 @@ def handle_person():
 @app.route('/person', methods=['POST'])
 def handle_add_person():
     body = request.get_json()
+
+    if body is None:
+        raise APIException("Body cannont be empty", status_code=400)
+    if 'full_name' not in body:
+        raise APIException('You need to specify the name', status_code=400)
+    if 'email' not in body:
+        raise APIException('You need to specify the email', status_code=400)
+    if 'address' not in body:
+        raise APIException('You need to specify the address', status_code=400)
+    if 'phone' not in body:
+        raise APIException('You need to specify the phone', status_code=400)
+    
     person = Person(username=body['username'], email=body['email'], full_name=body['full_name'], address=body['address'], phone=body['phone'])
     db.session.add(person)
     db.session.commit()
     return jsonify(person.serialize()), 200
 
-# @app.route('/person/<int:id>', methods=['GET', 'PUT', 'DELETE'])
-# def handle_person_update():
-#     if request.method == 'PUT'
-#         body = request.get_json()
-#         Person.query.filter_by(id=id).update({})
-
-
+@app.route('/person/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_person(id: int):
+    if request.method == 'PUT':
+        body = request.get_json()
+        Person.query.filter_by(id=id).update({'full_name':body['full_name'], 'email':body['email'], 'address':body['address'], 'phone':body['phone']})
+        person = Person.query.get(id)
+        if person:
+            db.session.commit()
+            return jsonify(message=person.full_name + " with id" + str(id) + " was successfully updated"), 200
+        else:
+            return jsonify(message=" id: "+str(id)+"does not exist"), 404
+    
+    if request.method == 'GET':
+        person = Person.query.get(id)
+        if person:
+            return jsonify(person.serialize()), 200
+        else: 
+            return jsonify(message='Id ' + str(id) + ' does not exist')
+    
+    if request.method == 'DELETE':
+        person = Person.query.get(id)
+        if person:
+            db.session.delete(person)
+            db.session.commit()
+            return jsonify(message=person.full_name + " deleted")
+        else: 
+            return jsonify(message='Id ' + str(id) + ' does not exist')
+    
+    return 'Invalid Method', 404
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
